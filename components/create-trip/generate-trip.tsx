230abb2a105efdd446040b0cf2a7b95325614172
Thai Hua <huavietthai299@gmail.com>
2025-04-27 20:11:25 +0700

    final update

--- components/create-trip/generate-trip.tsx
+++ components/create-trip/generate-trip.tsx
@@ -66,7 +66,7 @@ const GenerateTripScreen = () => {
   }, []);
 
   const AI_PROMPT =
-    "Generate Travel Plan for Location: {location} for {totalDays} Days and {totalNight} Night for {traveler} with a {budget} budget with a Flight details, Flight Price with Booking url, Hotels options list with HotelName, Hotel address, Price, hotel image url, geo coordinates, rating, descriptions and Places to visit nearby with placeName, Place Details, Place Image Url, Geo Coordinates, ticket Pricing, Time to travel each of the location for {totalDays} days and {totalNight} night with each day plan with best time to visit in JSON format.";
+    'Generate a {totalDays} Day, {totalNight} Night travel plan JSON for {traveler} with a {budget} budget in {location}, strictly following this structure: { "tripDetails": { "budget", "duration", "location", "travelers" }, "flightDetails": { "arrivalCity", "estimatedFlightPrice": { "amount",  "currency" }, "exampleBookingUrl" }, "hotelOptions": [{ "hotelName", "hotelAddress", "price": { "currency", "perNight" }, "hotelImageUrl", "geoCoordinates": { "latitude", "longitude" }, "rating", "description", "amenities": [] }], "dayPlans": [{ "day", "activities": [{ "placeName", "placeDetails", "placeImageUrl", "geoCoordinates": { "latitude", "longitude" }, "ticketPricing": { "amount", "currency" }, "timeToTravel", "bestTimeToVisit" }] }], "foodRecommendations": [], "notes": [] }, where "placeName" is a real location verifiable on Google Maps for accurate "geoCoordinates", using placeholder URLs/dates/prices, and outputting ONLY the JSON block.';
 
   const GenerateAITrip = async () => {
     setLoading(true);
--- components/trip-details/map.tsx
+++ components/trip-details/map.tsx
@@ -1,19 +1,19 @@
-import {
-  StyleSheet,
-  View,
-} from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import MapView, {
-  Marker,
-  PROVIDER_DEFAULT,
-  Region,
-} from "react-native-maps";
+import MapView, { Marker, PROVIDER_DEFAULT, Region } from "react-native-maps";
 import * as Location from "expo-location";
 import "react-native-get-random-values";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { NavigationProp, RouteProp, useNavigation, useRoute } from "@react-navigation/native";
+import {
+  NavigationProp,
+  RouteProp,
+  useNavigation,
+  useRoute,
+} from "@react-navigation/native";
+import PlaceCard from "./PlaceCard";
+import { GetGeoCoordinates } from "../../services/GooglePlaceAPI";
 
 const styles = StyleSheet.create({
   container: {
@@ -39,11 +39,30 @@ const MapScreen = () => {
   const route: RouteProp<RootStackParamList, "map"> = useRoute();
   const loc: any = route.params;
 
+  const mapRef = useRef<any>();
+  const focusMap = () => {
+    if (allCoordinates.length > 0 && allCoordinates[0]?.coordinates) {
+      const place = {
+        latitude: allCoordinates[0].coordinates.latitude,
+        longitude: allCoordinates[0].coordinates.longitude,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      };
+
+      mapRef.current?.animateCamera(
+        { center: place, zoom: 10 },
+        { duration: 3000 }
+      );
+    }
+  };
+
+  const [tripDetails, setTripDetails] = useState<any>();
+
   const [region, setRegion] = useState<Region>({
-    latitude: loc.latitude,
-    longitude: loc.longitude,
-    latitudeDelta: 0.005,
-    longitudeDelta: 0.005,
+    latitude: 11.9404192,
+    longitude: 108.4583132,
+    latitudeDelta: 5,
+    longitudeDelta: 5,
   });
 
   const userLocation = async () => {
@@ -52,7 +71,7 @@ const MapScreen = () => {
       console.log("Oops permission to access location was denied");
     }
 
-    // let location = await Location.getCurrentPositionAsync({});
+    let location = await Location.getCurrentPositionAsync({});
 
     // setRegion({
     //   latitude: location.coords.latitude,
@@ -62,41 +81,97 @@ const MapScreen = () => {
     // });
   };
 
+  const [allCoordinates, setAllCoordinates] = useState<any[]>([]);
+
+  const GetGoogleGeoCoordinates = async (placeName: string) => {
+    const result = await GetGeoCoordinates(placeName);
+    setAllCoordinates((prevCoordinates) => [
+      ...prevCoordinates,
+      {
+        name: placeName,
+        coordinates: {
+          latitude: result?.lat,
+          longitude: result?.lng,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        },
+      },
+    ]);
+  };
+
+  function getAllCoordinates() {
+    const placeNames: string[] = [];
+    if (tripDetails) {
+      tripDetails.tripPlan.dayPlans.forEach((dayPlan: any) => {
+        dayPlan.activities.forEach((activity: any) => {
+          placeNames.push(activity.placeName);
+        });
+      });
+      placeNames.forEach((name) => GetGoogleGeoCoordinates(name));
+    }
+  }
+
   useEffect(() => {
     userLocation();
+    setTripDetails(JSON.parse(loc));
   }, []);
 
-  return (
-    <SafeAreaView edges={["top"]} style={styles.container}>
-      <View style={styles.container}>
-      <Ionicons
-          style={styles.back_icon}
-          name="arrow-back"
-          size={24}
-          color="black"
-          onPress={() => navigation.goBack()}
-        />
+  useEffect(() => {
+    getAllCoordinates();
+  }, [tripDetails]);
 
-        <MapView
-          style={styles.map}
-          initialRegion={region}
-          region={region}
-          provider={PROVIDER_DEFAULT}
-          // onRegionChangeComplete={(region) => setRegion(region)}
-          zoomEnabled={true}
-          followsUserLocation
-          showsMyLocationButton
-          mapType="standard"
-          showsUserLocation
-        >
-          <Marker
-            coordinate={region}
-            title={loc.placeName}
-            // description={"VKU"}
+  useEffect(() => {
+    setTimeout(focusMap, 500);
+  }, [allCoordinates]);
+
+  return (
+    tripDetails &&
+    allCoordinates && (
+      <SafeAreaView edges={["top"]} style={styles.container}>
+        <View style={styles.container}>
+          <Ionicons
+            style={styles.back_icon}
+            name="arrow-back"
+            size={24}
+            color="black"
+            onPress={() => navigation.goBack()}
           />
-        </MapView>
-      </View>
-    </SafeAreaView>
+
+          <MapView
+            key={`map_${allCoordinates.length}`}
+            style={styles.map}
+            initialRegion={region}
+            region={region}
+            provider={PROVIDER_DEFAULT}
+            // onRegionChangeComplete={(region) => setRegion(region)}
+            zoomEnabled={true}
+            followsUserLocation
+            showsMyLocationButton
+            mapType="standard"
+            showsUserLocation
+            ref={mapRef}
+          >
+            {allCoordinates.map((val, index) => (
+              <Marker
+                title={val.name}
+                key={index}
+                coordinate={val.coordinates}
+                description={
+                  val.coordinates.latitude + " | " + val.coordinates.longitude
+                }
+              ></Marker>
+            ))}
+          </MapView>
+        </View>
+        {/* {allCoordinates.map((val, index) => (
+          <View key={index}>
+            <Text>
+              {val.name}: {val.coordinates.latitude}, {val.coordinates.longitude}
+            </Text>
+          </View>
+        ))} */}
+      </SafeAreaView>
+    )
   );
 };
 
